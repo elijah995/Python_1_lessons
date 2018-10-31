bf7d8054a57430292020bf462b10cbad9d44706e
elijah995 <dkeg95@yandex.ru>
2018-10-31 11:22:22 +0300

    Сделано hw03

--- lesson_03/home_work/hw03_hard.py
+++ lesson_03/home_work/hw03_hard.py
@@ -10,6 +10,36 @@
 # Вывод: 1 1/3
 
 
+s = input().split()
+x1 = [int(i) for i in s[0].split('/')]
+x1.append(1) # на случай, если первое слагаемое целое
+x2 = [int(i) for i in s[2].split('/')]
+x2.append(1)
+char = int(s[1] + '1')
+denom = x1[1] * x2[1]
+num = x1[0] * x2[1] + char * x2[0] * x1[1]
+if num < 0:
+    char = -1
+    num = abs(num)
+else:
+    char = 1
+a = num
+while a != 1:
+    if num % a == 0 and denom % a == 0:
+        num = num // a
+        denom = denom // a
+    a -= 1
+if num % denom == 0:
+    print(char * (num // denom))
+elif num > denom:
+    integ = char * (num // denom)
+    num = num % denom
+    print("{} {}/{}".format(integ, num, denom))
+else:
+    print("{}/{}".format(char * num, denom))
+
+
+print()
 # Задание-2:
 # Дана ведомость расчета заработной платы (файл "data/workers").
 # Рассчитайте зарплату всех работников, зная что они получат полный оклад,
@@ -18,7 +48,51 @@
 # они получают удвоенную ЗП, пропорциональную норме.
 # Кол-во часов, которые были отработаны, указаны в файле "data/hours_of"
 
+import os
 
+workers = []
+with open(".\data\workers", "r", encoding="utf8") as fin:
+    fin.readline()
+    for line in fin:
+        line = line.strip().split()
+        workers.append(line)
+hours = []        
+with open(".\data\hours_of", "r", encoding="utf8") as fin:
+    c
+    for line in fin:
+        line = line.strip().split()
+        hours.append(line)
+
+workers = sorted(workers, key=(lambda x: x[1]))
+hours = sorted(hours, key=(lambda x: x[1]))
+
+salaries = []
+for w, h in zip(workers, hours):
+    if int(h[2]) == int(w[4]):
+        salaries.append([h[0], h[1], h[2], w[2]])
+    elif int(h[2]) < int(w[4]):
+        s = round(int(h[2]) / int(w[4]) * int(w[2]), 2)
+        salaries.append([h[0], h[1], '{}-{}={}'.format(w[4], int(w[4]) - int(h[2]), h[2]), str(s)])
+    else:
+        s = round((int(h[2]) / int(w[4]) - 1) * int(w[2]) * 2, 2)
+        salaries.append([h[0], h[1], '{}+{}={}'.format(w[4], int(h[2]) - int(w[4]), h[2]), str(int(w[2]) + s)])
+
+for string in workers:
+    print('\t\t'.join(string))
+print()
+for string in hours:
+    print('\t\t'.join(string))
+print()
+for string in salaries:
+    print('\t\t'.join(string))
+
+s = [["Имя", "Фамилия", "Отработано", "Зарплата"]]
+s.extend(salaries)
+with open('.\data\salaries', "w", encoding="utf8") as fout:
+    for string in s:
+        fout.write('\t\t'.join(string) + "\n")
+
+print()
 # Задание-3:
 # Дан файл ("data/fruits") со списком фруктов.
 # Записать в новые файлы все фрукты, начинающиеся с определенной буквы.
@@ -31,3 +105,19 @@
 # Подсказка:
 # Чтобы получить список больших букв русского алфавита:
 # print(list(map(chr, range(ord('А'), ord('Я')+1))))
+
+from collections import defaultdict
+
+fruits = defaultdict(list)
+with open(r'.\data\fruits.txt', 'r', encoding="utf8") as fin:
+    for line in fin:
+        if line != '\n':
+            line = line.strip()
+            fruits[line[0]].append(line + '\n')
+            
+for letter in fruits.keys():
+    with open(r'.\data\fruits_' + letter + '.txt', "w", encoding="utf8") as fout:
+        for fruit in fruits[letter]:
+            fout.write(fruit)
+
+
--- lesson_03/home_work/hw03_normal.py
+++ lesson_03/home_work/hw03_normal.py
@@ -2,16 +2,72 @@
 # Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
 # Первыми элементами ряда считать цифры 1 1
 
+def fibonacci(n, m):
+    f = [1, 1]
+    f_nm = []
+    for i in range(0, m + 1):
+        if i >= n:
+            f_nm.append(f[i])
+        f.append(f[i] + f[i + 1])
+    return f_nm       
+
+
+print(fibonacci(1, 1))
+print(fibonacci(1, 10))
+print(fibonacci(3, 20))
+print(fibonacci(0, 5))
+
+print()
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
 # Для сортировки используйте любой алгоритм (например пузырьковый).
 # Для решения данной задачи нельзя использовать встроенную функцию и метод sort()
 
+def l_sort(l):
+    l_sorted = []
+    for i in range(len(l)):
+        l_sorted.append(l.pop(l.index(min(l))))
+    return l_sorted
+
+l = [9, 23, 45, 21, 109, -1, 93, -14]
+print(l)
+print(l_sort(l))
+
+print()
 # Задача-3:
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
 
+def filtered(cond, lst):
+    filtered = []
+    for item in lst:
+        if cond(item) == True:
+            filtered.append(item)
+    return filtered
+
+l = range(20)
+print(filtered(lambda x: x > 5, l))
+print(filtered(lambda x: x ** 2 in l, l))
+
+import math
+
+print(filtered(lambda x: math.sqrt(x) in l, l))
+    
+
+print()
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
+
+def parallelogram(a, b, c, d):
+    if b[0] - a[0] ==  c[0] - d[0] and b[1] - a[1] == c[1] - d[1]\
+    and d[0] - a[0] == c[0] - b[0] and d[1] - a[1] == c[1] - b[1]:
+        return True
+    else:
+        return False
+
+a1, a2, a3, a4 = [3, 2], [5, 7], [11, 8], [9, 3]
+print(parallelogram(a1, a2, a3, a4))
+      
+    
 
