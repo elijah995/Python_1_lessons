adbf126c53b2d638358b4751ed57d48786225ad3
elijah995 <dkeg95@yandex.ru>
2018-11-10 05:54:00 +0300

    дз 5

--- lesson_05/home_work/hw05_normal.py
+++ lesson_05/home_work/hw05_normal.py
@@ -15,10 +15,70 @@
 # и импортированные в данный файл из easy.py
 
 
+import os
+import sys
+#import hw05_easy as easy
+
+#print('sys.argv = ' , sys.argv)
+
+def print_help():
+    print("help - получение справки")
+    print("go_to - перейти в директорию")
+    print("show_content - просмотреть содержимое текущей директории")
+    print("rmdir - удалить директорию")
+    print("mkdir - создать директорию")
+
+def go_to():
+    dir_name = input("Укажите имя директории для перехода: ")
+    try:
+        os.chdir(dir_name)
+        print('Успешно перешёл в директорию {}'.format(dir_name))
+    except FileNotFoundError:
+        print('Невозможно перейти в директорию: директория {} не существует'.format(dir_name))
+        
+def show_content():
+    lst = os.listdir()
+    for el in lst:
+        print(el)
+        
+def mk_dir():
+    dir_name = input("Укажите имя создаваемой директории: ")
+    dir_path = os.path.join(os.getcwd(), dir_name)
+    try:
+        os.mkdir(dir_path)
+        print('Директория {} успешно создана'.format(dir_name))
+    except FileExistsError:
+        print('Невозможно создать директорию: директория {} уже существует'.format(dir_name))
+
+def rm_dir():
+    dir_name = input("Укажите имя удаляемой директории: ")
+    dir_path = os.path.join(os.getcwd(), dir_name)
+    try:
+        os.rmdir(dir_path)
+        print('Директория {} успешно удалена'.format(dir_name))
+    except FileNotFoundError:
+        print('Невозможно удалить директорию: директория {} не существует'.format(dir_name))
 
 
+        
+do = {
+    "help": print_help,
+    "go_to": go_to,
+    "show_content": show_content,
+    "mkdir": mk_dir,
+    "rmdir": rm_dir
+}
 
-
+try:
+    key = sys.argv[1]
+except IndexError:
+    key = None
+if key:
+    if do.get(key):
+        do[key]()
+    else:
+        print("Задан неверный ключ")
+        print("Укажите ключ help для получения справки")
 
 
 
